55a68906434d9812c3ec2ba8bd6a9b88dddc8e27
Brendan Rogers <brendanr11@gmail.com>
2019-03-19 13:28:53 -0400

    Added the app template, basic data structure, and displayed some content

---
+++ data.js
@@ -0,0 +1,42 @@
+const data = {
+    name: "GROOVETOWN",
+    role: "A Social Music Sharing Platform",
+
+    feature1: {
+        name: "TAGS",
+        brief: "Customize your music experience",
+        desc: "Filter by artist, genre, mood, listener demographic, or any custom tag you can think of!",
+        featPic: "tags.png"
+
+    },
+
+    feature2: {
+        name: "STORIES",
+        brief: "What are your friends listening to?",
+        desc: "Just like Instagram and Snapchat let you see what your friends are seeing in real time, GrooveTown Stories lets you see what they're listening to.",
+        featPic: "stories.png"
+    },
+
+    feature3: {
+        name: "PROFILES",
+        brief: "A little more about you...",
+        desc: "Let the world into your personal heavy rotation. Either set it yourself, or let your streaming service fill in the gaps.",
+        featPic: "profiles.png"
+    },
+
+    feature4: {
+        name: "THE MAP",
+        brief: "Explore the world in music",
+        desc: "See what people all over the world are listening to, in real time!",
+        featPic: "map.png"
+    },
+
+    feature5: {
+        name: "PLAYLISTS",
+        brief: "The sound of the city",
+        desc: "GrooveTown generates playlist for geographical areas automatically. Wondering what the kids are listening to from your old highschool? Find out!",
+        featPic: "playlists.png"
+    }
+};
+
+module.exports = data;
